a5eb8185af09e1d24d940639e0f4e5f0a4e5ad68
Carlos Coelho <carlos.c@quiltedhealth.com>
2025-08-15 13:31:21 +0000

    fix(EventWrapper): merge event color with additional styles for improved customization

--- src/Calendar/components/EventWrapper.jsx
+++ src/Calendar/components/EventWrapper.jsx
@@ -27,8 +27,11 @@ const EventWrapper = React.forwardRef(
       className={getEventContainerClass({ className, eventClass })}
       role="button"
       ref={ref}
-      style={event.color ? { backgroundColor: event.color, '--event-color': event.color } : {}}
-      onClick={(e) => {
+      style={{
+        ...(event.color ? { backgroundColor: event.color, '--event-color': event.color } : {}),
+        ...restProps.style
+      }}
+      onClick={e => {
         e.stopPropagation();
         if (!isSelectable) return false;
         onSelect({ e, event: resetEventFormat(event) });
